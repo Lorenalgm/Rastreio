d09e7b8c13e649ea82abebe0814013da2aa1c897
Lorenalgm <lorena@proesc.com>
2020-10-04 00:58:17 -0300

    Limpada estrutura e criada rota

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
+function Routes(){
+    return(
+        <BrowserRouter>
+            <Switch>
+                {/* <Route path= /> */}
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
